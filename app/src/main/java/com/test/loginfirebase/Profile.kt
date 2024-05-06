db5235c57415d5608243cdf299fb90a839585d6c
Shoaib <noorali5242@gmail.com>
2024-05-06 16:32:29 +0530

    feat add profile activity

---
+++ app/src/main/java/com/test/loginfirebase/Profile.kt
@@ -0,0 +1,23 @@
+package com.test.loginfirebase
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Profile : AppCompatActivity() {
+
+    private lateinit var imageBack : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        imageBack = findViewById(R.id.imageBack)
+        imageBack.setOnClickListener{
+            finish()
+        }
+
+    }
+}
