0a7e537df032600e430bdef837d1c3930e4da044
Shoaib <noorali5242@gmail.com>
2024-05-06 16:31:03 +0530

    feat add timeStamp

--- app/src/main/java/com/test/loginfirebase/data/Message.kt
+++ app/src/main/java/com/test/loginfirebase/data/Message.kt
@@ -1,15 +1,19 @@
 package com.test.loginfirebase.data
 
+import java.util.Date
+
 class Message {
 
      var message: String? = null
     var senderId: String? = null
+    var timeStamp: Long? = null
 
     constructor() {}
 
     constructor(message: String?, senderId: String?) {
         this.message = message
         this.senderId = senderId
+        this.timeStamp = Date().time
 
     }
 
