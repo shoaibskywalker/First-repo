972a23dec5d4a2fc1ef23b7848083915ff37dee6
Shoaib <noorali5242@gmail.com>
2024-09-05 15:19:53 +0530

    feat remove profile implementation

--- app/src/main/java/com/test/loginfirebase/Profile.kt
+++ app/src/main/java/com/test/loginfirebase/Profile.kt
@@ -12,6 +12,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.test.loginfirebase.databinding.ActivityHelpBinding
+import com.test.loginfirebase.databinding.ActivityProfileBinding
 import com.test.loginfirebase.utils.sessionManager.UserSessionManager
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -26,81 +28,13 @@ class Profile : AppCompatActivity() {
     private lateinit var prefs: UserSessionManager
 
     private val PICK_IMAGE_REQUEST = 1
+    private lateinit var binding: ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        prefs = UserSessionManager(this)
 
-        imageBack = findViewById(R.id.imageBack)
-        bgImage = findViewById(R.id.backgroundImage)
-        profileImg = findViewById(R.id.profileImage)
-        name = findViewById(R.id.textName)
-        email = findViewById(R.id.textEmail)
-        editImage = findViewById(R.id.editImage)
-        Log.d("sharedProfile", prefs.userEmailLogin.toString())
-        val emaill = intent.getStringExtra("login")
-email.text = emaill
-        //Baack
-        imageBack.setOnClickListener {
-            finish()
-        }
-
-        //background Image
-        bgImage.setOnLongClickListener {
-
-            true
-        }
-
-        editImage.setOnClickListener {
-            selectImage()
-        }
-
-    }
-
-    protected fun selectImage() {
-        val items = arrayOf<CharSequence>(
-            "Take Photo", "Choose from Library",
-            "Cancel"
-        )
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Add Photo!")
-        builder.setItems(items) { dialog, item ->
-            when {
-                (items[item] == "Take Photo") -> {
-                    showToast("Coming soon")
-                }
-
-                (items[item] == "Choose from Library") -> {
-                    //showToast("Coming soon")
-                    pickImageFromGallery()
-                }
-
-                (items[item] == "Cancel") -> dialog.dismiss()
-            }
-        }
-        builder.show()
-
-    }
-
-    private fun pickImageFromGallery() {
-        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
-            val imageUri = data.data
-            profileImg.setImageURI(imageUri)
-            // Save the selected image URI in shared preferences
-            prefs.userProfilePicture = imageUri.toString()
-            // You can also store the image in your app's storage or upload it to a server
-        }
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
