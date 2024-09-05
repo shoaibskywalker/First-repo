98cc4b9ca5bc9de71abda18082a55eae7fc36c8d
Shoaib <noorali5242@gmail.com>
2024-09-05 15:16:58 +0530

    feat add dependency for video call

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("io.agora.rtc:full-sdk:4.0.1")
 }
