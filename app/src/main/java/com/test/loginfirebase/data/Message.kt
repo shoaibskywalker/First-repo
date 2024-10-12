9e767a42bf6d2fb2bd57775bf556e993228ffeb0
Shoaib <noorali5242@gmail.com>
2024-10-12 16:10:55 +0530

    feat update message data model

--- app/src/main/java/com/test/loginfirebase/data/Message.kt
+++ app/src/main/java/com/test/loginfirebase/data/Message.kt
@@ -2,23 +2,9 @@ package com.test.loginfirebase.data
 
 import java.util.Date
 
-class Message {
-
-     var message: String? = null
-    var senderId: String? = null
-    var timeStamp: Long? = null
+data class Message(
+    var message: String? = null,
+    var senderId: String? = null,
+    var timeStamp: Long? = System.currentTimeMillis(),
     var messageId: String = ""
-
-
-    constructor() {}
-
-    constructor(message: String?, senderId: String?) {
-        this.message = message
-        this.senderId = senderId
-        this.timeStamp = Date().time
-        var messageId: String = ""
-
-
-    }
-
-}
+)
