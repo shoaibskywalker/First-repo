c8368b26375db8808a93652a268bbe8bccf0adcc
Shoaib <noorali5242@gmail.com>
2024-10-12 16:11:07 +0530

    feat update user data model

--- app/src/main/java/com/test/loginfirebase/data/User.kt
+++ app/src/main/java/com/test/loginfirebase/data/User.kt
@@ -1,20 +1,13 @@
 package com.test.loginfirebase.data
 
-class User {
+class User (
 
-    var name: String? = null
-    var email: String? = null
-    var uid: String? = null
+    var name: String? = null,
+    var email: String? = null,
+    var uid: String? = null,
+    var profileImageUrl: String? = null
+)
 
-    constructor() {}
-
-    constructor(name: String?, email: String?, uid: String?) {
-        this.name = name
-        this.email = email
-        this.uid = uid
-    }
-
-}
 
 
 
