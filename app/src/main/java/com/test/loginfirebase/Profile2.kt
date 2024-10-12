531a2b63dad074ae6b5af31440652b3784c9c7f5
Shoaib <noorali5242@gmail.com>
2024-10-12 16:14:16 +0530

    feat add image upload work

--- app/src/main/java/com/test/loginfirebase/Profile2.kt
+++ app/src/main/java/com/test/loginfirebase/Profile2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,11 +13,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.test.loginfirebase.databinding.ActivityProfile2Binding
 import com.test.loginfirebase.databinding.BagListBinding
+import com.test.loginfirebase.utils.sessionManager.UserSessionManager
 
 class Profile2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfile2Binding
     private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
+    private lateinit var userSessionManager: UserSessionManager
+    private var currentUserEmail: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +32,19 @@ class Profile2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        userSessionManager = UserSessionManager(this)  // Initialize session manager
+
+        currentUserEmail = userSessionManager.userEmailLogin
+
+        // Load the saved image when the activity starts
+        currentUserEmail?.let { email ->
+            // Load the saved image for this user when the activity starts
+            val savedImage = userSessionManager.getUserProfileImage(email)
+            savedImage?.let {
+                binding.image.setImageBitmap(it)
+            }
         }
 
         binding.imageBack.setOnClickListener {
@@ -42,15 +60,21 @@ class Profile2 : AppCompatActivity() {
                     // Using MediaStore to access the image
                     val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
                     binding.image.setImageBitmap(bitmap)  // Display in ImageView
+                    Log.d("selected image", uri.toString())
+                    // Save the selected image to SharedPreferences
+                    // Save the selected image for the current user
+                    currentUserEmail?.let { email ->
+                        userSessionManager.saveUserProfileImage(email, bitmap)
+                    }
                 }
             }
         }
 
-        binding.ImageUpload.setOnClickListener {
+        binding.imageEdit.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             intent.type = "image/*"
             pickImageLauncher.launch(intent)
         }
-
+        binding.emailProfile.text = userSessionManager.userEmailLogin
     }
 }
