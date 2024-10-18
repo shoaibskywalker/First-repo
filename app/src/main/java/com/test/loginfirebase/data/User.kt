f14eaee11e0c7dd1140a2844f1758f21e7edc162
Shoaib <noorali5242@gmail.com>
2024-10-18 11:20:20 +0530

    feat add more variables

--- app/src/main/java/com/test/loginfirebase/data/User.kt
+++ app/src/main/java/com/test/loginfirebase/data/User.kt
@@ -5,7 +5,9 @@ class User (
     var name: String? = null,
     var email: String? = null,
     var uid: String? = null,
-    var profileImageUrl: String? = null
+    var profileImageUrl: String? = null,
+    var lastChatTimestamp: Long = 0L, // Default value
+    var userFcmToken: String? = null
 )
 
 
