55c3820d960c7d9022df6e3ffae870e79c7075e9
Shoaib <noorali5242@gmail.com>
2024-10-18 12:40:20 +0530

    feat rename file

--- app/src/main/java/com/test/loginfirebase/fcm/FcmServerToken.kt
+++ app/src/main/java/com/test/loginfirebase/fcm/FcmServerToken.kt
@@ -2,7 +2,10 @@ package com.test.loginfirebase.fcm
 
 import android.content.Context
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.remoteConfig
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class ServerToken(private val context: Context) {
@@ -14,10 +17,26 @@ class ServerToken(private val context: Context) {
         if (cachedToken != null) {
             return@withContext cachedToken!!
         }
+        // Fetch the service account JSON from Firebase Remote Config
+        val remoteConfig = Firebase.remoteConfig
+
+        // Set default values if needed (can be skipped if using Firebase console defaults)
+        remoteConfig.setDefaultsAsync(mapOf("service_account_json" to ""))
+
+        // Fetch the remote config (you can set a fetch interval for caching, like 3600 seconds)
+        remoteConfig.fetchAndActivate().await()
+
+        // Get the service account JSON from Remote Config
+        val serviceAccountJson = remoteConfig.getString("service_credential")
+
+        if (serviceAccountJson.isEmpty()) {
+            throw IllegalArgumentException("Service account JSON is missing in Remote Config")
+        }
 
         val assetManager = context.assets
 
-        val inputStream = assetManager.open("newService.json")
+        val inputStream = serviceAccountJson.byteInputStream()
+
 
 // Load credentials and generate the access token
         val credentials: GoogleCredentials = com.google.auth.oauth2.ServiceAccountCredentials
