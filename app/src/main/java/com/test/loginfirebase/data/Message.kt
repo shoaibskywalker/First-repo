550b976fd904cf050e45685aa35d373f1fc6e0c5
Shoaib <noorali5242@gmail.com>
2024-10-27 20:09:41 +0530

    feat add date stamp

--- app/src/main/java/com/test/loginfirebase/data/Message.kt
+++ app/src/main/java/com/test/loginfirebase/data/Message.kt
@@ -6,5 +6,6 @@ data class Message(
     var message: String? = null,
     var senderId: String? = null,
     var timeStamp: Long? = System.currentTimeMillis(),
+    var dateStamp: Long? = System.currentTimeMillis(),
     var messageId: String = ""
 )
