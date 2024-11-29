83bce96a275e4f9cf740e15e776e5c945576c551
Shoaib <noorali5242@gmail.com>
2024-11-29 12:16:16 +0530

    feat add zegocloud,story view  dependency and remove unnecessary dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
         exclude ("META-INF/NOTICE")
         exclude ("META-INF/NOTICE.txt")
     }
+    buildToolsVersion = "28.0.3"
 }
 
 dependencies {
@@ -56,8 +57,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.activity:activity:1.8.0")
@@ -71,21 +70,18 @@ dependencies {
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("io.agora.rtc:full-sdk:4.0.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation("com.google.ai.client.generativeai:generativeai:0.2.0")
-    implementation ("androidx.room:room-runtime:2.5.0")
-    annotationProcessor ("androidx.room:room-compiler:2.5.0")
-    implementation ("androidx.room:room-ktx:2.5.0")
     implementation ("com.google.android.gms:play-services-auth:20.6.0")
     implementation ("com.google.auth:google-auth-library-oauth2-http:1.2.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation ("com.google.firebase:firebase-config-ktx:21.0.5")
     implementation("androidx.biometric:biometric:1.1.0")
-
-
+    implementation ("com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:+")
+    implementation ("com.github.OMARIHAMZA:StoryView:1.0.2-alpha")
+    implementation ("androidx.work:work-runtime-ktx:2.7.1")
 
 }
