19f4e86bb65fad9cf8c4b7c046880526b3fb6857
Shoaib <noorali5242@gmail.com>
2024-11-29 12:00:08 +0530

    feat remove unnecessary imports

--- app/src/main/java/com/test/loginfirebase/ForgotPasswordActivity.kt
+++ app/src/main/java/com/test/loginfirebase/ForgotPasswordActivity.kt
@@ -3,11 +3,7 @@ package com.test.loginfirebase
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.test.loginfirebase.databinding.ActivityForgotPasswordBinding
 import com.test.loginfirebase.utils.CommonUtil
