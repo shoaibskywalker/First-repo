758e661f179d0a5f77e8290f76fea1b87a303a3b
Shoaib <noorali5242@gmail.com>
2024-11-29 11:56:05 +0530

    feat add fun for save about

--- app/src/main/java/com/test/loginfirebase/utils/sessionManager/UserSessionManager.kt
+++ app/src/main/java/com/test/loginfirebase/utils/sessionManager/UserSessionManager.kt
@@ -2,10 +2,6 @@ package com.test.loginfirebase.utils.sessionManager
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.util.Base64
-import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,7 +13,7 @@ class UserSessionManager
 
     private val prefEditor: SharedPreferences.Editor = prefs.edit()
 
-    fun addUnreadUser(context: Context, userId: String) {
+    fun addUnreadUser(userId: String) {
         val unreadUsers = prefs.getStringSet("KEY_UNREAD_USERS", mutableSetOf())?.toMutableSet()
         unreadUsers?.add(userId)
         prefEditor.putStringSet("KEY_UNREAD_USERS", unreadUsers).apply()
@@ -30,49 +26,46 @@ class UserSessionManager
     }
 
 
-    fun getUnreadUsers(context: Context): Set<String> {
+    fun getUnreadUsers(): Set<String> {
         return prefs.getStringSet("KEY_UNREAD_USERS", mutableSetOf()) ?: mutableSetOf()
     }
 
-    // Method to save the image as a Base64 string in SharedPreferences
 
-    fun saveUserProfileImage(userEmail: String, bitmap: Bitmap) {
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
-        val imageBytes = baos.toByteArray()
-        val imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT)
-        prefEditor.putString("${userEmail}_profile_image", imageString)
-        prefEditor.apply()
-    }
-
-    fun getUserProfileImage(userEmail: String): Bitmap? {
-        val imageString = prefs.getString("${userEmail}_profile_image", null)
-        return imageString?.let {
-            val imageBytes = Base64.decode(it, Base64.DEFAULT)
-            BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    var currentUserPicture: String
+        get() = prefs.getString("currentUserPicture", "") ?: ""
+        set(value) {
+            prefs.edit().putString("currentUserPicture", value).apply()
         }
+
+
+     fun saveReceiverProfilePictureUrl(userId: String, url: String) {
+        prefEditor.putString("profile_picture_url_$userId", url) // Use userId as part of the key
+        prefEditor.apply() // or editor.commit()
     }
 
-    // Clear the saved image when user logs out
-    fun clearUserProfileImage(userEmail: String) {
-        prefEditor.remove("${userEmail}_profile_image")
-        prefEditor.apply()
+     fun getReceiverProfilePictureUrl(userId: String): String? {
+        return prefs.getString("profile_picture_url_$userId", null) // Use the same key structure
     }
+
+    private fun saveAboutText(text: String) {
+        prefEditor.putString("aboutText", text)
+        prefEditor.apply()
+        }
+
+
+
+    var userAbout: String?
+        get() = prefs.getString("userAbout", null)
+        set(userName) {
+            prefEditor.putString("userAbout", userName)
+            prefEditor.apply()
+        }
 
     var userEmailLogin: String
         get() = prefs.getString("userEmailLogin", "") ?: ""
         set(value) {
             prefs.edit().putString("userEmailLogin", value).apply()
         }
-
-
-    var clearTimestamp: Long
-        get() = prefs.getLong("clear_timestamp", 0L)
-        set(value) {
-            prefEditor.putLong("clear_timestamp", value)
-            prefEditor.apply()
-        }
-
 
     var userEmailSignup: String?
         get() = prefs.getString("userEmailSignup", null)
@@ -107,14 +100,4 @@ class UserSessionManager
         prefEditor.apply()
     }
 
-    fun saveString(key: String, value: String) {
-        val editor = prefs.edit()
-        editor.putString(key, value)
-        editor.apply()
-    }
-
-    // Method to retrieve a string value from shared preferences
-    fun getString(key: String, defaultValue: String): String {
-        return prefs.getString(key, defaultValue) ?: defaultValue
-    }
 }
