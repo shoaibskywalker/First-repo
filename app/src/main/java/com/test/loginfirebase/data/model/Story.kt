de2ac5ed49ba1b4f447a2d440ee2fa8180a33a3f
noora <noorali5242@gmail.com>
2025-01-08 13:51:26 +0530

    feat refactor status to story

--- app/src/main/java/com/test/loginfirebase/data/model/Story.kt
+++ app/src/main/java/com/test/loginfirebase/data/model/Story.kt
@@ -1,7 +1,7 @@
 package com.test.loginfirebase.data.model
 
 
-data class Status(
+data class Story(
     val imageUrl: String = "",
     val name: String = "",
     val timestamp: Long = System.currentTimeMillis(), // Optional timestamp for ordering
