aca6f5503491f9d19b865f891035e11a300cd021
noora <noorali5242@gmail.com>
2025-01-08 13:59:46 +0530

    feat add qr library zxing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,16 +35,16 @@ android {
         jvmTarget = "1.8"
     }
 
-    buildFeatures{
+    buildFeatures {
         viewBinding = true
     }
 
     packagingOptions {
-        exclude ("META-INF/DEPENDENCIES")
-        exclude ("META-INF/LICENSE")
-        exclude ("META-INF/LICENSE.txt")
-        exclude ("META-INF/NOTICE")
-        exclude ("META-INF/NOTICE.txt")
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/LICENSE")
+        exclude("META-INF/LICENSE.txt")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/NOTICE.txt")
     }
     buildToolsVersion = "28.0.3"
 }
@@ -58,32 +58,35 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.activity:activity:1.8.0")
-    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.google.firebase:firebase-storage-ktx:21.0.0")
     implementation("com.google.firebase:firebase-messaging:24.0.2")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("com.google.firebase:firebase-appdistribution-gradle:5.0.0")
     testImplementation("junit:junit:4.13.2")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation("com.google.ai.client.generativeai:generativeai:0.2.0")
-    implementation ("com.google.android.gms:play-services-auth:20.6.0")
-    implementation ("com.google.auth:google-auth-library-oauth2-http:1.2.2")
+    implementation("com.google.android.gms:play-services-auth:20.6.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.2.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation ("com.google.firebase:firebase-config-ktx:21.0.5")
+    implementation("com.google.firebase:firebase-config-ktx:21.0.5")
     implementation("androidx.biometric:biometric:1.1.0")
-    implementation ("com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:+")
-    implementation ("com.github.OMARIHAMZA:StoryView:1.0.2-alpha")
-    implementation ("androidx.work:work-runtime-ktx:2.7.1")
-    implementation ("com.airbnb.android:lottie:6.0.0")
-    implementation ("com.github.stfalcon-studio:StfalconImageViewer:v1.0.1")
+    implementation("com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:+")
+    implementation("com.github.OMARIHAMZA:StoryView:1.0.2-alpha")
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("com.airbnb.android:lottie:6.0.0")
+    implementation("com.github.stfalcon-studio:StfalconImageViewer:v1.0.1")
+    implementation("com.google.zxing:core:3.5.2")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0") // for integration with Android
+
 
 }
